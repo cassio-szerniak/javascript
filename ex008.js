dbba692c0e4056e858e988ed87a4a70a11194320
Cassio Szerniak <cassioszerniak@gmail.com>
2021-10-21 00:48:32 -0300

    20/10/21

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 66
+console.log(`A velocidade do seu carro é ${vel}km`)
+if (vel > 66) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija usando cinto de segurança!`)
